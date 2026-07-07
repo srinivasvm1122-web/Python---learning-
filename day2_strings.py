0a401664df8ed3ca9b07cc432b72a659adb91cda
Srinivas <srinivasvm1122@gmail.com>
2026-07-07 23:33:00 +0530

    Add day2_strings.py with basic data types

---
+++ day2_strings.py
@@ -0,0 +1,19 @@
+# Day 2 - Data Types
+# Author: Srinivas
+
+# Integer
+age = 20
+print("Age:", age)
+print("Type:", type(age))
+
+# Float
+weight = 65.5
+print("Weight:", weight)
+
+# String
+name = "Srinivas"
+print("Name:", name)
+
+# Boolean
+is_student = True
+print("Is student:", is_student)
